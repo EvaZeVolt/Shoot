989b8bb824c135613a67bd502a8a5f2b0e3ce0b2
EvaZeVolt <71860413+EvaZeVolt@users.noreply.github.com>
2020-10-28 08:04:13 -0400

    more

--- js/Controller.js
+++ js/Controller.js
@@ -1,10 +1,16 @@
-var Player, controller, loop, Blocks, Title;
+var Player, controller, loop, Blocks, Blocks2, Title, turkey;
 var ctx = document.getElementById("canvas").getContext("2d");
 
 ctx.canvas.height = 470;
 ctx.canvas.width = 999;
 //title
 
+turkey = {
+  width: 50,
+  height: 50,
+  x: 300,
+  y: 450
+};
 Title = {
   height: 50,
   width: 50
@@ -19,13 +25,19 @@ Blocks = {
   y: 350
 };
 
+Blocks2 = {
+  height: 50,
+  width: 200,
+  x: 450,
+  y: 250
+};
 //game
 
 Player = {
   height: 50,
   width: 50,
   x: 500,
-  y: 300,
+  y: 350,
   x_velocity: 0,
   y_velocity: 0
 };
@@ -53,7 +65,7 @@ controller = {
 
 loop = function () {
   if (controller.up && Player.jumping == false) {
-    Player.y_velocity -= 30;
+    Player.y_velocity -= 27;
     Player.jumping = true;
   }
 
@@ -88,7 +100,22 @@ loop = function () {
     Player.y = 300;
     Player.y_velocity = 0;
   }
+  //2block
+  if (Player.y > 202 && Player.y < 220 && Player.x > 400 && Player.x < 648) {
+    Player.jumping = false;
+    Player.y = 202;
+    Player.y_velocity = 0;
+  }
+
+  if (Player.y > 202 && Player.y < 220 && Player.x > 400 && Player.x < 648) {
+    Player.jumping = false;
+    Player.y = 350;
+  }
+
   //end
+  ctx.fillStyle = "gray";
+  ctx.fillRect(400, 200, 200, 200);
+
   //Floor
   if (Player.y > 450 - 16 - 32) {
     Player.jumping = false;
@@ -107,10 +134,13 @@ loop = function () {
   ctx.fillStyle = "#38A2DF";
   ctx.fillRect(0, 0, 999, 500);
   //end of fllor
+
   ctx.fillStyle = "#ff0000";
   ctx.beginPath();
   ctx.rect(Player.x, Player.y, Player.width, Player.height);
+  ctx.fillStyle = "Red";
   ctx.fill();
+
   ctx.strokeStyle = "#202830";
   ctx.lineWidth = 100;
   ctx.beginPath();
@@ -118,12 +148,22 @@ loop = function () {
   ctx.lineTo(999, 500);
   ctx.stroke();
 
+//turkey
+
+
+
   //blocks
+
   ctx.fillStyle = "#363836";
   ctx.stroke();
   ctx.fillRect(Blocks.x, Blocks.y, Blocks.width, Blocks.height);
 
+  ctx.fillStyle = "#363836";
+  ctx.fillRect(Blocks2.x, Blocks2.y, Blocks2.width, Blocks2.height);
+
   window.requestAnimationFrame(loop);
+
+  //text
 };
 
 //Images
